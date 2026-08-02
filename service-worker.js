88939af22134af82463405884514e195607f9017
bootscootinlinedancing <bootscootin.linedancing@yahoo.com>
2026-08-02 14:58:19 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,13 @@
-const CACHE_NAME = "boot-scootin-v56";
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match(e.request)))});
+const CACHE_NAME = "boot-scootin-v57";
+self.addEventListener("install", event => self.skipWaiting());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request, {cache:"no-store"}).catch(() => caches.match(event.request)));
+});
