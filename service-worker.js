e15ae5319cf0b9c2ec561c17a9109019afb5b314
bootscootinlinedancing <bootscootin.linedancing@yahoo.com>
2026-08-02 15:25:17 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "boot-scootin-v59";
+const CACHE_NAME = "boot-scootin-v60";
 self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => {
   event.waitUntil(
@@ -9,8 +9,5 @@ self.addEventListener("activate", event => {
 });
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request, {cache:"no-store"})
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request, {cache:"no-store"}).catch(() => caches.match(event.request)));
 });
